1777b6e08ce8efa6355d2f23f3606479d6687eab
rosanass <rohsana0912>
2018-11-21 16:54:18 -0300

    creating system

---
+++ Main.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+
+public class Main
+{
+    public static void main(String[] args)
+    {
+        int aux = 1;
+        while (aux != 0)
+        {
+            Scanner input = new Scanner(System.in);
+            System.out.println("Escolha a opção desejada:");
+            System.out.println("[0] Sair");
+            System.out.println("[1] Cadastrar novo empregado");
+            System.out.println("[2] Remover empregado");
+            System.out.println("[3] Lançar um Cartão de Ponto");
+            System.out.println("[4] Lançar um Resultado Venda");
+            System.out.println("[5] Lançar uma taxa de serviço");
+            System.out.println("[6] Alterar detalhes de um empregado");
+            System.out.println("[7] Rodar a folha de pagamento para hoje");
+            System.out.println("[8] Undo/redo");
+            System.out.println("[9] Agenda de Pagamento");
+            System.out.println("[10] Criação de Novas Agendas de Pagamento");
+            int opcao = input.nextInt();
+            aux = opcao;
+
+            switch(opcao)
+            {
+                case 1;
+
+            }
+        }
+
+    }
+
+}
